02caeee9d780fa39714094209ebdf0f3d6dd30a0
Jason figueiredo <119349812+Jfigs12@users.noreply.github.com>
2024-09-11 19:52:19 -0400

    Overall save

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #include "../Model/PHIModel.h"
 #include "../LiteDB/LiteDB.h"
 #include "../IntakeParsing/intake.h"
+#include "../LiteDB/AppendtoDB.h"
 
 
 bool setPHIfromMain(personalInfo &temp,string name, string address, bool includeOffices, int phoneNumber,int PersonalID){
@@ -29,12 +30,12 @@ void checkSetDB(){
 int main(){
     printf("test1");
     personalInfo p1;
+    Representative rep1;
     setPHIfromMain(p1, "John Doe", "1234 Elm St", true, 1234567890, 1);
     checkSetDB();
     bool test = commitPHI(p1);
     printf("test2");
-    setPointertoCSV();
-
+    commitReptoDB(3, rep1);
     
     return 0;
     
