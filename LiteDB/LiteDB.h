6ac0cba168ae1cf6f3fb0c3ef147429e597a442a
Jason figueiredo <119349812+Jfigs12@users.noreply.github.com>
2024-09-11 18:55:16 -0400

    yay

--- LiteDB/LiteDB.h
+++ LiteDB/LiteDB.h
@@ -7,14 +7,14 @@
 #include <sqlite3.h>
 using namespace std;
 #include "../Model/PHIModel.h"
-
+#include "../Model/representativeModel.h"
 
 bool DBFunction();
 bool closeDB(sqlite3 *db);
 bool createTablePHI3(sqlite3 *db);
 bool commitPHI(personalInfo &temp);
 bool createTableRepresentative(sqlite3* DB);
-
+bool commitRepresentativeModel(Representative &rep, int personID);
 
 
 
